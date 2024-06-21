1b3ac7865b7557ce8962d55b7b232a58154c9903
unknown <DAC@KN-7E7IT0H.kemenkeu.go.id>
2024-06-21 08:46:21 +0700

    Routes | Add barang

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from .models import Barang
 from . import db
 
@@ -37,3 +37,45 @@ def get_listbarang():
         "response": output
     }
     return jsonify(response)
+
+# Endpoint untuk menambahkan data barang
+@bp.route('/barang', methods=['POST'])
+def add_barang():
+    data = request.get_json()
+
+    if not data or not 'kode' in data or not 'name' in data or not 'harga' in data or not 'jumlah' in data:
+        return jsonify({
+            "statusCode": 400,
+            "message": "Data tidak lengkap",
+            "response": None
+        }), 400
+
+    new_barang = Barang(
+        kode=data['kode'],
+        name=data['name'],
+        harga=data['harga'],
+        jumlah=data['jumlah']
+    )
+
+    try:
+        db.session.add(new_barang)
+        db.session.commit()
+    except Exception as e:
+        db.session.rollback()
+        return jsonify({
+            "statusCode": 500,
+            "message": f"Kesalahan dalam menambahkan data: {str(e)}",
+            "response": None
+        }), 500
+
+    response = {
+        "statusCode": 201,
+        "message": "Data barang berhasil ditambahkan",
+        "response": {
+            "kode": new_barang.kode,
+            "name": new_barang.name,
+            "harga": new_barang.harga,
+            "jumlah": new_barang.jumlah
+        }
+    }
+    return jsonify(response), 201
